baaae7243895333f182e2cd9d81d822f8562e8a1
rohdesamuel <rohde.samuel@gmail.com>
2017-12-25 19:25:42 -0600

    add event class to replace channel

---
+++ src/event.cpp
@@ -0,0 +1,68 @@
+#include "event.h"
+#include "system_impl.h"
+
+#include <cstring>
+
+Event::Event(
+  qbId id,
+  ByteQueue* message_queue,
+  size_t size)
+  : id_(id),
+    message_queue_(message_queue),
+    size_(size),
+    mem_buffer_(size),
+    free_mem_(size) {
+  mem_buffer_.reserve(1000);
+  free_mem_.reserve(1000);
+}
+
+Event::Message Event::AllocMessage(void* initial_val) {
+  size_t ret_index = 0;
+  if (free_mem_.empty()) {
+    ret_index = mem_buffer_.size();
+    mem_buffer_.push_back(initial_val);
+    initial_val = nullptr;
+  } else {
+    ret_index = free_mem_.back();
+    free_mem_.pop_back();
+  }
+  if (initial_val) {
+    void* val = mem_buffer_[ret_index];
+    memmove(val, initial_val, size_);
+  }
+  return{ id_, ret_index };
+}
+
+qbResult Event::SendMessage(void* message) {
+  Event::Message new_message = AllocMessage(message);
+  message_queue_->push(&new_message);
+  return qbResult::QB_OK;
+}
+
+qbResult Event::SendMessageSync(void* message) {
+  for (const auto& handler : handlers_) {
+    (SystemImpl::FromRaw(handler))->Run(message);
+  }
+
+  return qbResult::QB_OK;
+}
+
+void Event::AddHandler(qbSystem s) {
+  handlers_.push_back(s);
+}
+
+void Event::RemoveHandler(qbSystem s) {
+  handlers_.erase(std::find(handlers_.begin(), handlers_.end(), s));
+}
+
+void Event::Flush(size_t index) {
+  for (const auto& handler : handlers_) {
+    void* m = (uint8_t*)(Message*)mem_buffer_[index] + size_;
+    SystemImpl::FromRaw(handler)->Run(m);
+  }
+  FreeMessage(index);
+}
+
+void Event::FreeMessage(size_t index) {
+  free_mem_.push_back(index);
+}
---
+++ src/event.h
@@ -0,0 +1,54 @@
+#ifndef EVENT__H
+#define EVENT__H
+
+#include "concurrentqueue.h"
+#include "defs.h"
+#include "byte_vector.h"
+#include "byte_queue.h"
+#include "memory_pool.h"
+
+#include <mutex>
+#include <queue>
+#include <set>
+
+class Event {
+ public:
+  struct Message {
+    qbId handler;
+    size_t index;
+  };
+
+  Event(qbId id, ByteQueue* message_queue, size_t size = 1);
+
+  // Thread-safe.
+  qbResult SendMessage(void* message);
+
+  // Thread-safe.
+  qbResult SendMessageSync(void* message);
+
+  // Not thread-safe.
+  void AddHandler(qbSystem s);
+
+  // Not thread-safe.
+  void RemoveHandler(qbSystem s);
+
+  // Not thread-safe.
+  void Flush(size_t index);
+
+ private:
+  // Allocates a message to send. Moves the data pointed to by initial_val
+  // to a new message. Returns pointer to the newly allocated message.
+   Message AllocMessage(void* initial_val);
+
+  // Thread-safe.
+  void FreeMessage(size_t index);
+
+  std::vector<qbSystem> handlers_;
+  qbId id_;
+  ByteQueue* message_queue_;
+  size_t size_;
+  ByteVector mem_buffer_;
+  std::vector<size_t> free_mem_;
+};
+
+#endif  // EVENT__H
