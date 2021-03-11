4eb5b9b1cbf81e6670e78d2480df6c7e77a13008
Samuel Rohde <rohde.samuel@gmail.com>
2021-03-11 02:23:13 -0800

    Remove qbAnimator struct, replaced with qbSprite

--- include/cubez/sprite.h
+++ include/cubez/sprite.h
@@ -23,26 +23,58 @@
 #include <cglm/struct.h>
 
 typedef struct qbSprite_* qbSprite;
-typedef struct qbSpriteAtlas_* qbSpriteAtlas;
-
-QB_API qbSpriteAtlas qb_spriteatlas_load(const char* atlas_name, const char* filename, int tw, int th);
+typedef struct qbAnimationBuilder_* qbAnimationBuilder;
+typedef struct qbAnimation_* qbAnimation;
+typedef struct qbAnimator_* qbAnimator;
 
 QB_API qbSprite qb_sprite_load(const char* sprite_name, const char* filename);
 
-QB_API qbSprite qb_sprite_fromatlas(const char* sprite_name, qbSpriteAtlas atlas, int ix, int iy);
+QB_API qbSprite qb_spritesheet_load(const char* sheet_name, const char* filename, int tw, int th, int margin);
+
+QB_API qbSprite qb_sprite_fromsheet(const char* sprite_name, qbSprite sheet, int ix, int iy);
 
 QB_API qbSprite qb_sprite_find(const char* sprite_name);
 
-QB_API void     qb_sprite_draw(qbSprite sprite, int32_t subimg, vec2s pos);
+QB_API void     qb_sprite_draw(qbSprite sprite, vec2s pos);
 
-QB_API void     qb_sprite_draw_ext(qbSprite sprite, int32_t subimg, vec2s pos,
+QB_API void     qb_sprite_draw_ext(qbSprite sprite, vec2s pos,
                                    vec2s scale, float rot, vec4s col);
+
+QB_API void     qb_sprite_setoffset(qbSprite sprite, vec2s offset);
+
+QB_API uint32_t qb_sprite_width(qbSprite sprite);
+QB_API uint32_t qb_sprite_height(qbSprite sprite);
 
 QB_API void     qb_sprite_setdepth(qbSprite sprite, float depth);
 QB_API float    qb_sprite_getdepth(qbSprite sprite);
 
 QB_API void     qb_sprite_initialize(uint32_t width, uint32_t height);
 QB_API void     qb_sprite_onresize(uint32_t width, uint32_t height);
-QB_API void     qb_sprite_flush(qbFrameBuffer frame);
+QB_API void     qb_sprite_flush(qbFrameBuffer frame, qbRenderEvent e);
+
+typedef struct qbAnimationAttr_ {
+  qbSprite* frames;
+
+  // Unit in milliseconds.
+  double* durations;
+  size_t frame_count;
+
+  // Unit in milliseconds.
+  double frame_speed;
+
+  bool repeat;
+  int keyframe;
+} qbAnimationAttr_, *qbAnimationAttr;
+
+QB_API qbAnimation qb_animation_create(const char* animation_name, qbAnimationAttr attr);
+
+QB_API qbAnimation qb_animation_fromsheet(const char* animation_name,
+                                          qbAnimationAttr attr, qbSprite sheet,
+                                          int index_start, int index_end);
+
+QB_API qbSprite  qb_animation_play(qbAnimation animation);
+
+// Component type: qbSprite
+QB_API qbComponent qb_sprite();
 
 #endif  // CUBEZ_SPRITE__H
--- src/sprite.cpp
+++ src/sprite.cpp
@@ -6,14 +6,18 @@
 #include <algorithm>
 #include <iterator>
 #include <set>
+#include <string>
 #include <unordered_map>
 #include <vector>
 
 
-constexpr int MAX_BATCH_TEXTURE_UNITS = 32;
+constexpr int MAX_BATCH_TEXTURE_UNITS = 31;
 constexpr size_t SPRITE_VERTEX_ATTRIBUTE_SIZE =
-  // Position
-  3 +
+  // Vertex
+  2 +
+
+  // Offset
+  2 +
 
   // Color
   4 +
@@ -29,6 +33,7 @@ constexpr size_t SPRITE_VERTEX_ATTRIBUTE_SIZE =
 
   // Texture Id
   1;
+constexpr size_t MAX_NUM_SPRITES_PER_BATCH = 1000;
 
 // std140 layout
 struct UniformCamera {
@@ -45,27 +50,38 @@ enum RenderMode {
   GUI_RENDER_MODE_STRING,
 };
 
+struct qbAnimator_ {
+  qbAnimation animation;
+  int frame;
+  double elapsed;
+};
+
 struct qbSprite_ {  
   qbImage img;
+
+  vec2s offset;
 
   // Index of sprite in atlas, -1 if not from atlas.
   int ix, iy;
 
   // Width and height of the sprite in pixels.
-  int w, h;
+  uint32_t w, h;
+
+  // Width and height of the tile.
+  uint32_t tw, th;
+
+  uint32_t margin;
 
   // Depth of sprite for painter's algorithm.
   float depth;
 
-  qbSpriteAtlas opt_atlas;
-};
-
-struct qbSpriteAtlas_ {
-  int w, h;
+  // If the sprite is animated, this will point to the animation.  
+  qbAnimator_ animator;
 };
 
 struct QueuedSprite {
   qbSprite sprite;
+  float depth;
 
   // Position to draw at.
   vec2s pos;
@@ -79,12 +95,27 @@ struct QueuedSprite {
   // RGBA color.
   vec4s col;
 
+  // If the sprite is part of an animation, then this will point to its
+  // animator.
+  qbAnimator animator;
+
   bool operator<(const QueuedSprite& other) {
-    if (sprite->depth == other.sprite->depth) {
+    if (depth == depth) {
       return this < &other;
     }
-    return sprite->depth < other.sprite->depth;
+    return depth < other.depth;
   }
+};
+
+struct qbAnimation_ {
+  std::vector<qbSprite> frames;
+  std::vector<double> durations;
+
+  double frame_duration;
+  bool repeat;
+  int keyframe;
+
+  uint32_t w, h;
 };
 
 struct Batch {
@@ -104,6 +135,34 @@ qbRenderPass sprite_render_pass;
 qbRenderGroup batched_sprites;
 qbMeshBuffer sprite_quads;
 
+// A 1x1 white texture that is used to render solid color sprites.
+qbImage clear_texture;
+
+qbComponent sprite_component;
+
+qbSprite qb_spritesheet_load(const char* sheet_name, const char* filename, int tw, int th, int margin) {
+  qbSprite sheet = qb_sprite_load(sheet_name, filename);
+  sheet->tw = tw;
+  sheet->th = th;
+  sheet->margin = margin;
+
+  return sheet;
+}
+
+qbSprite qb_sprite_fromsheet(const char* sprite_name, qbSprite sheet, int ix, int iy) {
+  qbSprite ret = new qbSprite_{};
+  ret->w = sheet->w;
+  ret->h = sheet->h;
+  ret->img = sheet->img;
+  ret->margin = sheet->margin;
+  ret->tw = sheet->tw;
+  ret->th = sheet->th;
+  ret->ix = ix;
+  ret->iy = iy;
+
+  return ret;
+}
+
 qbSprite qb_sprite_load(const char* sprite_name, const char* filename) {
   qbImage img{};
   qbImageAttr_ attr{};
@@ -114,6 +173,8 @@ qbSprite qb_sprite_load(const char* sprite_name, const char* filename) {
   qbSprite ret = new qbSprite_{};
   ret->w = qb_image_width(img);
   ret->h = qb_image_height(img);
+  ret->tw = ret->w;
+  ret->th = ret->h;
   ret->img = img;
 
   return ret;
@@ -123,15 +184,22 @@ qbSprite qb_sprite_find(const char* sprite_name) {
   return nullptr;
 }
 
-void qb_sprite_draw(qbSprite sprite, int32_t subimg, vec2s pos) {
-  sprites.push_back({ sprite });
+void qb_sprite_draw_internal(qbSprite sprite, vec2s pos, vec2s scale, float rot, vec4s col) {
+  if (sprite->animator.animation) {
+    qbSprite frame = sprite->animator.animation->frames[sprite->animator.frame];
+    sprites.push_back({ frame, sprite->depth, pos, scale, rot, col, &sprite->animator });
+  } else {
+    sprites.push_back({ sprite, sprite->depth, pos, scale, rot, col });
+  }
   std::push_heap(sprites.begin(), sprites.end());
 }
 
-void qb_sprite_draw_ext(qbSprite sprite, int32_t subimg, vec2s pos,
-                        vec2s scale, float rot, vec4s col) {
-  sprites.push_back({ sprite });
-  std::push_heap(sprites.begin(), sprites.end());
+void qb_sprite_draw(qbSprite sprite, vec2s pos) {
+  qb_sprite_draw_internal(sprite, pos, GLMS_VEC2_ONE_INIT, 0.f, GLMS_VEC4_ONE_INIT);
+}
+
+void qb_sprite_draw_ext(qbSprite sprite, vec2s pos, vec2s scale, float rot, vec4s col) {
+  qb_sprite_draw_internal(sprite, pos, scale, rot, col);
 }
 
 void qb_sprite_setdepth(qbSprite sprite, float depth) {
@@ -142,8 +210,101 @@ float qb_sprite_getdepth(qbSprite sprite) {
   return sprite->depth;
 }
 
-void qb_sprite_resize(qbSprite sprite) {
+void qb_sprite_setoffset(qbSprite sprite, vec2s offset) {
+  sprite->offset = offset;
+}
 
+uint32_t qb_sprite_width(qbSprite sprite) {
+  return sprite->w;
+}
+
+uint32_t qb_sprite_height(qbSprite sprite) {
+  return sprite->h;
+}
+
+qbAnimation qb_animation_create(const char* animation_name, qbAnimationAttr attr) {
+  qbAnimation animation = new qbAnimation_{};
+  uint32_t width = 0;
+  uint32_t height = 0;
+
+  for (size_t i = 0; i < attr->frame_count; ++i) {
+    if (attr->frames) {
+      animation->frames.push_back(attr->frames[i]);
+      width = std::max(width, attr->frames[i]->w);
+      height = std::max(height, attr->frames[i]->h);
+    }
+    if (attr->durations) {
+      animation->durations.push_back(attr->durations[i] / 1000.0);
+    }
+  }
+  animation->repeat = attr->repeat;
+  animation->keyframe = attr->keyframe;
+  animation->frame_duration = attr->frame_speed / 1000.0;
+  animation->w = width;
+  animation->h = height;
+  return animation;
+}
+
+qbAnimation qb_animation_fromsheet(const char* animation_name, qbAnimationAttr attr, qbSprite sheet,
+                                   int index_start, int index_end) {
+  std::vector<qbSprite> frames;
+  std::vector<double> durations;
+
+  int iw = sheet->w / sheet->tw;
+  for (int i = index_start; i < index_end; ++i) {
+    int ix = i % iw;
+    int iy = i / iw;
+    frames.push_back(qb_sprite_fromsheet(nullptr, sheet, ix, iy));
+  }
+  
+  for (int i = 0; i < attr->frame_count; ++i) {
+    durations.push_back(attr->durations[i]);
+  }
+
+  if (attr->frame_count > frames.size()) {
+    for (int i = 0; i < frames.size() - attr->frame_count; ++i) {
+      durations.push_back(0.0);
+    }
+  }
+
+  attr->frames = frames.data();
+  attr->durations = durations.data();
+  attr->frame_count = frames.size();
+
+  return qb_animation_create(animation_name, attr);
+}
+
+qbSprite qb_animation_play(qbAnimation animation) {
+  qbSprite ret = new qbSprite_{};
+  ret->w = animation->w;
+  ret->h = animation->h;
+
+  ret->animator.animation = animation;
+  ret->animator.frame = animation->keyframe;
+  return ret;
+}
+
+void qb_animator_update(qbAnimator animator, qbRenderEvent e) {
+  qbAnimation animation = animator->animation;
+
+  if (animation->frames.empty()) {
+    return;
+  }
+
+  animator->elapsed += e->dt;
+  double frame_time = animation->durations.empty() ? animation->frame_duration : animation->durations[animator->frame];
+  if (animator->elapsed >= frame_time) {
+    ++animator->frame;
+    animator->elapsed -= frame_time;
+  }
+
+  if (animator->frame >= animation->frames.size()) {
+    if (animation->repeat) {
+      animator->frame = animation->keyframe;
+    } else {
+      --animator->frame;
+    }
+  }
 }
 
 qbRenderPass sprite_create_renderpass(uint32_t width, uint32_t height) {
@@ -154,42 +315,42 @@ qbRenderPass sprite_create_renderpass(uint32_t width, uint32_t height) {
   binding.stride = SPRITE_VERTEX_ATTRIBUTE_SIZE * sizeof(float);
   binding.input_rate = QB_VERTEX_INPUT_RATE_VERTEX;
 
-  qbVertexAttribute_ attributes[6] = {};
+  qbVertexAttribute_ attributes[7] = {};
   {
-    // Position
+    // Vertex
     qbVertexAttribute_* attr = attributes;
     attr->binding = 0;
     attr->location = 0;
 
-    attr->count = 3;
+    attr->count = 2;
     attr->type = QB_VERTEX_ATTRIB_TYPE_FLOAT;
     attr->normalized = false;
     attr->offset = (void*)0;
   }
   {
-    // Color
+    // Offset
     qbVertexAttribute_* attr = attributes + 1;
     attr->binding = 0;
     attr->location = 1;
 
-    attr->count = 4;
+    attr->count = 2;
     attr->type = QB_VERTEX_ATTRIB_TYPE_FLOAT;
     attr->normalized = false;
-    attr->offset = (void*)(3 * sizeof(float));
+    attr->offset = (void*)(2 * sizeof(float));
   }
   {
-    // UVs
+    // Color
     qbVertexAttribute_* attr = attributes + 2;
     attr->binding = 0;
     attr->location = 2;
 
-    attr->count = 2;
+    attr->count = 4;
     attr->type = QB_VERTEX_ATTRIB_TYPE_FLOAT;
     attr->normalized = false;
-    attr->offset = (void*)(7 * sizeof(float));
+    attr->offset = (void*)(4 * sizeof(float));
   }
   {
-    // Scale
+    // UVs
     qbVertexAttribute_* attr = attributes + 3;
     attr->binding = 0;
     attr->location = 3;
@@ -197,21 +358,21 @@ qbRenderPass sprite_create_renderpass(uint32_t width, uint32_t height) {
     attr->count = 2;
     attr->type = QB_VERTEX_ATTRIB_TYPE_FLOAT;
     attr->normalized = false;
-    attr->offset = (void*)(9 * sizeof(float));
+    attr->offset = (void*)(8 * sizeof(float));
   }
   {
-    // Rotation
+    // Scale
     qbVertexAttribute_* attr = attributes + 4;
     attr->binding = 0;
     attr->location = 4;
 
-    attr->count = 1;
+    attr->count = 2;
     attr->type = QB_VERTEX_ATTRIB_TYPE_FLOAT;
     attr->normalized = false;
-    attr->offset = (void*)(11 * sizeof(float));
+    attr->offset = (void*)(10 * sizeof(float));
   }
   {
-    // Texture Id
+    // Rotation
     qbVertexAttribute_* attr = attributes + 5;
     attr->binding = 0;
     attr->location = 5;
@@ -220,6 +381,17 @@ qbRenderPass sprite_create_renderpass(uint32_t width, uint32_t height) {
     attr->type = QB_VERTEX_ATTRIB_TYPE_FLOAT;
     attr->normalized = false;
     attr->offset = (void*)(12 * sizeof(float));
+  }
+  {
+    // Texture Id
+    qbVertexAttribute_* attr = attributes + 6;
+    attr->binding = 0;
+    attr->location = 6;
+
+    attr->count = 1;
+    attr->type = QB_VERTEX_ATTRIB_TYPE_FLOAT;
+    attr->normalized = false;
+    attr->offset = (void*)(13 * sizeof(float));
   }
 
   qbShaderModule shader_module;
@@ -235,12 +407,15 @@ qbRenderPass sprite_create_renderpass(uint32_t width, uint32_t height) {
       resources.push_back(info);
     }
 
-    for (uint32_t i = 0; i < MAX_BATCH_TEXTURE_UNITS; ++i) {
+    std::vector<std::string> resource_names;
+    resource_names.reserve(MAX_BATCH_TEXTURE_UNITS + 1);
+    for (uint32_t i = 0; i < MAX_BATCH_TEXTURE_UNITS + 1; ++i) {
+      resource_names.push_back(std::string("tex_sampler[") + std::to_string(i) + "]");
       qbShaderResourceInfo_ info;
       info.binding = 1 + i;
       info.resource_type = QB_SHADER_RESOURCE_TYPE_IMAGE_SAMPLER;
       info.stages = QB_SHADER_STAGE_FRAGMENT;
-      info.name = "tex_sampler";
+      info.name = resource_names.back().c_str();
 
       resources.push_back(info);
     }
@@ -275,20 +450,24 @@ qbRenderPass sprite_create_renderpass(uint32_t width, uint32_t height) {
     }
   }
   {
-    qbImageSampler image_samplers[1];
-    {
+    std::vector<uint32_t> bindings;
+    std::vector<qbImageSampler> image_samplers;
+    for (uint32_t i = 0; i < MAX_BATCH_TEXTURE_UNITS + 1; ++i) {
+      bindings.push_back(1 + i);
+
       qbImageSamplerAttr_ attr = {};
+      qbImageSampler sampler;
       attr.image_type = QB_IMAGE_TYPE_2D;
-      attr.min_filter = QB_FILTER_TYPE_LINEAR;
-      attr.mag_filter = QB_FILTER_TYPE_LINEAR;
+      attr.min_filter = QB_FILTER_TYPE_NEAREST;
+      attr.mag_filter = QB_FILTER_TYPE_NEAREST;
       attr.s_wrap = QB_IMAGE_WRAP_TYPE_REPEAT;
       attr.t_wrap = QB_IMAGE_WRAP_TYPE_REPEAT;
-      qb_imagesampler_create(image_samplers, &attr);
+      qb_imagesampler_create(&sampler, &attr);
+
+      image_samplers.push_back(sampler);
     }
 
-    uint32_t bindings[] = { 1 };
-    qbImageSampler* samplers = image_samplers;
-    qb_shadermodule_attachsamplers(shader_module, 1, bindings, samplers);
+    qb_shadermodule_attachsamplers(shader_module, bindings.size(), bindings.data(), image_samplers.data());
   }
 
   {
@@ -297,7 +476,7 @@ qbRenderPass sprite_create_renderpass(uint32_t width, uint32_t height) {
     attr.supported_geometry.bindings = &binding;
     attr.supported_geometry.bindings_count = 1;
     attr.supported_geometry.attributes = attributes;
-    attr.supported_geometry.attributes_count = 3;
+    attr.supported_geometry.attributes_count = sizeof(attributes) / sizeof(attributes[0]);
     attr.supported_geometry.mode = QB_DRAW_MODE_TRIANGLES;
     attr.shader = shader_module;
     attr.viewport = { 0.0, 0.0, (float)width, (float)height };
@@ -343,12 +522,28 @@ qbRenderPass sprite_create_renderpass(uint32_t width, uint32_t height) {
 
     qb_rendergroup_append(batched_sprites, sprite_quads);
   }
+  {
+    uint8_t white_pixel[4] = { 0xFF, 0xFF, 0xFF, 0xFF };
+    qbPixelMap white_pixelmap = qb_pixelmap_create(1, 1, 0, qbPixelFormat::QB_PIXEL_FORMAT_RGBA8, white_pixel);
+
+    qbImageAttr_ attr = {};
+    attr.type = qbImageType::QB_IMAGE_TYPE_2D;
+
+    qb_image_create(&clear_texture, &attr, white_pixelmap);
+  }
 
   return render_pass;
 }
 
 void qb_sprite_initialize(uint32_t width, uint32_t height) {
   sprite_render_pass = sprite_create_renderpass(width, height);
+
+  {
+    qbComponentAttr attr;
+    qb_componentattr_create(&attr);
+    qb_componentattr_setdatatype(attr, qbSprite);
+    qb_componentattr_destroy(&attr);
+  }
 }
 
 void qb_sprite_onresize(uint32_t width, uint32_t height) {
@@ -361,7 +556,7 @@ void qb_sprite_onresize(uint32_t width, uint32_t height) {
   qb_renderpass_resize(sprite_render_pass, { 0.0, 0.0, (float)width, (float)height });
 }
 
-void qb_sprite_flush(qbFrameBuffer frame) {
+void qb_sprite_flush(qbFrameBuffer frame, qbRenderEvent e) {
   if (sprites.empty()) {
     return;
   }
@@ -375,6 +570,12 @@ void qb_sprite_flush(qbFrameBuffer frame) {
     QueuedSprite s = std::move(sprites.front());
     std::pop_heap(sprites.begin(), sprites.end()); sprites.pop_back();
     
+    // Update the animator here, so that extra work to update them in a
+    // separate call isn't done.
+    if (s.animator) {
+      qb_animator_update(s.animator, e);
+    }
+
     batch.images.insert(s.sprite->img);
     batch.sprites.push_back(std::move(s));
     if (batch.images.size() >= MAX_BATCH_TEXTURE_UNITS) {
@@ -391,14 +592,19 @@ void qb_sprite_flush(qbFrameBuffer frame) {
   for (auto& batch : batches) {
     std::vector<float> vertices;
     std::vector<int> indices;
-    std::vector<uint32_t> texture_ids;
+    std::vector<uint32_t> texture_bindings;
     std::vector<qbImage> textures;
     std::unordered_map<qbImage, uint32_t> texture_to_ids;
 
+    for (int i = 0; i < MAX_BATCH_TEXTURE_UNITS + 1; ++i) {
+      texture_bindings.push_back(i + 1);
+      textures.push_back(clear_texture);
+    }
+    texture_to_ids[clear_texture] = 0;
+
     int texture_id = 1;
     for (qbImage img : batch.images) {
-      texture_ids.push_back(texture_id);
-      textures.push_back(img);
+      textures[texture_id] = img;
       texture_to_ids[img] = texture_id;
 
       ++texture_id;
@@ -408,42 +614,40 @@ void qb_sprite_flush(qbFrameBuffer frame) {
     for (auto& queued : batch.sprites) {
       float attributes[SPRITE_VERTEX_ATTRIBUTE_SIZE];
       uint32_t quad_indices[] = {
-        6 * index + 3, 6 * index + 1, 6 * index + 0,
-        6 * index + 2, 6 * index + 3, 6 * index + 0
+        4 * index + 3, 4 * index + 1, 4 * index + 0,
+        4 * index + 2, 4 * index + 3, 4 * index + 0
       };
 
       for (uint32_t i = 0; i < 4; ++i) {
         float x = i & 0x1;
         float y = (i & 0x2) >> 1;
         // Position
-        attributes[0] = queued.pos.x + queued.sprite->w * x;
-        attributes[1] = queued.pos.y + queued.sprite->h * y;
-        attributes[2] = 0.f;
+        attributes[0] = queued.sprite->tw * x - queued.sprite->offset.x;
+        attributes[1] = queued.sprite->th * y - queued.sprite->offset.y;
+
+        // Offset
+        attributes[2] = queued.pos.x;
+        attributes[3] = queued.pos.y;
 
         // Color
-        attributes[3] = queued.col.x;
-        attributes[4] = queued.col.y;
-        attributes[5] = queued.col.z;
-        attributes[6] = queued.col.w;
+        attributes[4] = queued.col.x;
+        attributes[5] = queued.col.y;
+        attributes[6] = queued.col.z;
+        attributes[7] = queued.col.w;
 
         // Texture ix/iy for atlas.
-        if (queued.sprite->opt_atlas) {
-          attributes[7] = ((queued.sprite->ix + x) * queued.sprite->w) / queued.sprite->opt_atlas->w;
-          attributes[8] = ((queued.sprite->iy + y) * queued.sprite->h) / queued.sprite->opt_atlas->h;
-        } else {
-          attributes[7] = x;
-          attributes[8] = y;
-        }
+        attributes[8] = (queued.sprite->ix + x) * ((float)(queued.sprite->tw + queued.sprite->margin) / (float)queued.sprite->w);
+        attributes[9] = (queued.sprite->iy + y) * ((float)(queued.sprite->th + queued.sprite->margin) / (float)queued.sprite->h);
 
         // Scale
-        attributes[9] = queued.scale.x;
-        attributes[10] = queued.scale.y;
+        attributes[10] = queued.scale.x;
+        attributes[11] = queued.scale.y;
 
         // Rotation
-        attributes[11] = queued.rot;
+        attributes[12] = queued.rot;
 
         // Texture Id
-        attributes[12] = texture_to_ids[queued.sprite->img];
+        attributes[13] = texture_to_ids[queued.sprite->img];
 
         std::copy(attributes, attributes + SPRITE_VERTEX_ATTRIBUTE_SIZE, std::back_inserter(vertices));        
       }
@@ -462,8 +666,12 @@ void qb_sprite_flush(qbFrameBuffer frame) {
     qb_gpubuffer_resize(index_buffer, indices.size() * sizeof(uint32_t));
     qb_gpubuffer_update(index_buffer, 0, indices.size() * sizeof(uint32_t), indices.data());
 
-    qb_meshbuffer_updateimages(sprite_quads, textures.size(), texture_ids.data(), textures.data());
+    qb_meshbuffer_updateimages(sprite_quads, textures.size(), texture_bindings.data(), textures.data());
     
     qb_renderpass_drawto(sprite_render_pass, frame, 1, &batched_sprites);
   }
+}
+
+qbComponent qb_sprite() {
+  return sprite_component;
 }
