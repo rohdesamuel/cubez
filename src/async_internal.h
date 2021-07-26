b6f95f3d07a5c4560c888731824c44dc7c8cacd1
Sam Rohde <rohde.samuel@gmail.com>
2021-07-25 22:16:32 -0700

    Add internal qb_taskbundle_addtask

--- src/async_internal.h
+++ src/async_internal.h
@@ -2,8 +2,13 @@
 #define ASYNC_INTERNAL__H
 
 #include <cubez/cubez.h>
+#include <cubez/async.h>
+#include <functional>
 
 void async_initialize(qbSchedulerAttr_* attr);
 void async_stop();
+
+
+void qb_taskbundle_addtask(qbTaskBundle bundle, std::function<qbVar(qbTask, qbVar)>&& entry, qbTaskBundleAddTaskInfo info);
 
 #endif  // ASYNC_INTERNAL__H
