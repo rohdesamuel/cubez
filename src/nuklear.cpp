0443750084157230fb42f73423a7dbb03db64d3f
Samuel Rohde <rohde.samuel@gmail.com>
2024-06-04 22:46:23 -0700

    Remove unused GUI methods replaced by Nuklear

--- src/nuklear.cpp
+++ src/nuklear.cpp
@@ -6,7 +6,6 @@
 #define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
 #define NK_INCLUDE_FONT_BAKING
 #define NK_INCLUDE_DEFAULT_FONT
-#define NK_IMPLEMENTATION
 #define NK_SDL_GL3_IMPLEMENTATION
 #include <cubez/nuklear.h>
 #include <iostream>
